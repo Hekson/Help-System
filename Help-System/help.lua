14ea8fc465d6041a4d5078eb418f4c0beebdb256
Hekson <105570804+Hekson@users.noreply.github.com>
2022-10-06 14:59:14 +0330

    Update help.lua

--- Help-System/help.lua
+++ Help-System/help.lua
@@ -1,10 +1,10 @@
 RegisterCommand("help", function()
-    msg("Discord : discord.gg/RmfQbvWaDc")
-    msg("Server Owner : Hekson & Mamad")
+    msg("Discord : https://discord.gg/ZpD9YCVwcS")
+    msg("Server Owner : Hekson")
     msg("Server Development : Hekson")
     msg("Starter : Starter Pack Dakhel Hesab Shoma Ast")
 end, false)
 
 function msg(text)
-    TriggerEvent("chatMessage", "[supporter]", {255,0,0}, text)
+    TriggerEvent("chatMessage", "[System]", {255,0,0}, text)
 end
